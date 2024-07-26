820f4d9fa5b47cc4bc6c77fe58bb6787a70a9c32
yegon-gk <kiprotych@gmail.com>
2024-07-26 22:48:30 +0300

    I created, configured and connected to  MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('You are connected to MongoDB !!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000 !!');
+  console.log('Server is listening on port 3000 !!!');
 });
