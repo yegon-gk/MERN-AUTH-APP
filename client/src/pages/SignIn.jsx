57e599c24a4e0d13f3f5f2e2a52b72763d521dd7
yegon-gk <kiprotych@gmail.com>
2024-07-31 13:03:43 +0300

    I added Redux toolkit & React-Redux packages and updated the error message functionality.

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({
@@ -8,9 +14,9 @@ export default function SignIn() {
     password: '',
   });
 
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -19,8 +25,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -29,15 +34,15 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
+
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error);
+      dispatch(signInFailure(error));
     }
   };
 
@@ -78,7 +83,7 @@ export default function SignIn() {
         </p>
       </div>
       <p className="text-red-700 mt-3 text-sm ">
-        {error && 'Enter the right credentials!!!'}
+        {error ? error.message || 'Oops!! Something went wrong.' : ''}
       </p>
     </div>
   );
